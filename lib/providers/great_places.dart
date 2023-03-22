10d065c98d804f62e2190889bc7d32124f0a9f3b
Luis020-hub <guhmonteiro10@hotmail.com>
2023-03-21 23:15:14 -0300

    21/03/2023
    - Criada a pasta models com o arquivo place.dart definindo as classes PlaceLocation e Place.
    - Criada a pasta providers e o arquivo great_places.dart, com os métodos do ChangeNotifier.
    - Instalando o provider na aplicação.

---
+++ lib/providers/great_places.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+
+import '../models/place.dart';
+
+class GreatPlaces with ChangeNotifier {
+  List<Place> _items = [];
+
+  List<Place> get items {
+    return [..._items];
+  }
+
+  int get itemsCount {
+    return _items.length;
+  }
+
+  Place getItem(int index) {
+    return _items[index];
+  }
+}
